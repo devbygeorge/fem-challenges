f978d065acd3753842627cc1e094d43a618cb4bf
GiorgiPasieshvili <giopasieshvili2@gmail.com>
2022-01-03 17:13:23 +0400

    Rest-Countries-API improve

--- Rest-Countries-API/src/index.js
+++ Rest-Countries-API/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import allReducer from './redux/reducers'
@@ -9,11 +9,9 @@ import allReducer from './redux/reducers'
 import App from './App';
 import './index.css';
 
-const composedEnhancer = compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
 const store = createStore(
   allReducer, 
-  composedEnhancer
+  applyMiddleware(thunk)
 );
 
 ReactDOM.render(
