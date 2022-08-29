c4d9597f2b9bb39310b8db092467d4fdcf54bcba
GiorgiPasieshvili <giopasieshvili2@gmail.com>
2022-08-29 11:52:53 +0400

    base-apparel-coming-soon optimize

---
+++ base-apparel-coming-soon/js/index.js
@@ -0,0 +1,28 @@
+const form = document.querySelector("#form");
+const messageArea = document.querySelector("#message");
+const regexp =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+form.addEventListener("submit", validateForm);
+
+function validateForm(e) {
+  e.preventDefault();
+  if (regexp.test(e.target[0].value)) {
+    e.target[0].value = "";
+    showAlert("success", "Email successfully added");
+  } else {
+    showAlert("failure", "Please provide a valid email");
+  }
+}
+
+function showAlert(type, message) {
+  messageArea.innerText = message;
+  messageArea.classList.remove("success");
+  messageArea.classList.remove("failure");
+  messageArea.classList.add(type);
+
+  setTimeout(() => {
+    messageArea.classList.remove(type);
+    messageArea.innerText = "";
+  }, 2000);
+}
