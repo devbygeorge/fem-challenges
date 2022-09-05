14b50031abe33e1dabcddc546143e9ee7db1a0a6
GiorgiPasieshvili <giopasieshvili2@gmail.com>
2022-09-05 17:48:42 +0400

    blogr upgrade part 1

--- blogr-landing-page/app/js/script.js
+++ blogr-landing-page/app/js/script.js
@@ -1,19 +1,18 @@
-const btnHamburger = document.querySelector("#btnHamburger");
-const overlay = document.querySelector(".overlay");
-const headerMenu = document.querySelector(".header__menu");
-const headerMenuItems = document.querySelectorAll(".header__item");
+const navToggle = document.querySelector("#toggle");
+const headerNav = document.querySelector("#header-nav");
 
-function toggleMenu() {
-  btnHamburger.classList.toggle("open");
-  overlay.classList.toggle("open");
-  headerMenu.classList.toggle("open");
-}
-
-headerMenuItems.forEach((item) => {
-  const button = item.firstElementChild;
-  button.addEventListener("click", () => {
-    item.classList.toggle("open");
-  });
+navToggle.addEventListener("click", () => {
+  headerNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true);
+  headerNav.toggleAttribute("data-visible");
 });
 
-btnHamburger.addEventListener("click", toggleMenu);
+// const headerMenuItems = document.querySelectorAll(".header__item");
+
+// headerMenuItems.forEach((item) => {
+//   const button = item.firstElementChild;
+//   button.addEventListener("click", () => {
+//     item.classList.toggle("open");
+//   });
+// });
