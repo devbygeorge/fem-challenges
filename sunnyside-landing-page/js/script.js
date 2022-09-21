f397fc9f22e71a4fb95ecd7312e784ddbf545812
GiorgiPasieshvili <giopasieshvili2@gmail.com>
2022-09-21 04:25:37 +0400

    sunnyside resolve accessibility issues

--- sunnyside-landing-page/js/script.js
+++ sunnyside-landing-page/js/script.js
@@ -1,6 +1,6 @@
 const mobileMenu = document.querySelector("#mobile-menu");
-const primaryNav = document.querySelector("#primary-nav");
+const headerNavList = document.querySelector("#header-nav-list");
 
 mobileMenu.addEventListener('click', () => {
-  primaryNav.classList.toggle('open');
+  headerNavList.classList.toggle("open");
 });
