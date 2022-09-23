5d30069c399db52eb88f1dd2cf9b6c0f89214a06
GiorgiPasieshvili <giopasieshvili2@gmail.com>
2022-09-24 02:35:34 +0400

    resolve accessebility issues, styling bugs and add comments to the js code  interactive-pricing-component

--- interactive-pricing-component/js/script.js
+++ interactive-pricing-component/js/script.js
@@ -6,30 +6,36 @@ const pricingFeeValue = document.querySelector("#js-pricing-fee-value");
 const pricingBillingType = document.querySelector("#js-pricing-billing-type");
 let isBillingSwitched = false;
 
-/* change billing type when switch button clicked */
+/* things to happen when pricing switch is toggled */
 pricingSwitch.onclick = () => {
-  /* set its class to active */
+  /* 1. toggle active class on the button */
   pricingSwitch.classList.toggle("active");
 
-  /* toggle year and month */
+  /* 2. toggle year and month near pricing fee */
   isBillingSwitched = !isBillingSwitched;
   pricingBillingType.textContent = isBillingSwitched ? "/year" : "/month";
 
-  /* change price simultaneously */
+  /* 3. change price value according to billing type */
   changePrice();
 };
 
+/* things to happen when pricing slider is triggered */
 pricingSelector.oninput = () => {
+  /* 1. change progress bar width */
   pricingProgressBar.style.width = pricingSelector.value + "%";
 
+  /* 2. change pageviews value */
   pricingPageviews.textContent = pricingSelector.value * 2 + "k pageviews";
 
+  /* 2. change price value */
   changePrice();
 };
 
 const changePrice = () => {
-  const changedPricingFeeValue = isBillingSwitched
-    ? pricingSelector.value / 4.15
-    : pricingSelector.value / 3.125;
+  /* we need to get maximum $32 for month and $25 for year */
+  let divideValue = isBillingSwitched ? 100 / 25 : 100 / 32;
+
+  /* 1% - 100% / $0.25 - $25.00 or $0.32 - $32.00 */
+  const changedPricingFeeValue = pricingSelector.value / divideValue;
   pricingFeeValue.textContent = "$" + changedPricingFeeValue.toFixed(2);
 };
