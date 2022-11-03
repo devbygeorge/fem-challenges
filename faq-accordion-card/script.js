7126f436ef6e334c4ee270b310166be6e53e6c7b
GiorgiPasieshvili <giopasieshvili2@gmail.com>
2022-11-03 14:53:02 +0400

    Optimize faq-accordion-card markup & css

--- faq-accordion-card/script.js
+++ faq-accordion-card/script.js
@@ -1,10 +1,8 @@
 document.addEventListener("click", toggleAnswer);
 
 function toggleAnswer(e) {
-  if (e.target.classList.contains("card__title")) {
+  if (e.target.classList.contains("accordion__title")) {
     const parentClassList = e.target.parentElement.classList;
-    parentClassList.contains("show")
-      ? parentClassList.remove("show")
-      : parentClassList.add("show");
+    parentClassList.toggle("show");
   }
 }
